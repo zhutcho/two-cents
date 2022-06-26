635493c450d272147a1d03eac4c3ff57387f8816
Zane <zah004@student.usc.edu.au>
2022-06-26 12:08:54 +1000

    TailwindCSS working properly

--- two-cents/src/App.vue
+++ two-cents/src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './views/Dashboard.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
   <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <Dashboard msg="Yessir"/>
 </template>
 
 <style>
--- two-cents/src/views/Dashboard.vue
+++ two-cents/src/views/Dashboard.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+    defineProps<{ msg: string }>()
 </script>
 
 <template>
-    
+    <h1 class="italic font-bold hover:not-italic text-2xl"> {{ msg }} </h1>
 </template>
 
 <style scoped>
